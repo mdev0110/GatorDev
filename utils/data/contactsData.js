a8d632d82920ef3a851eb1b623e50ae1661204d2
Darijo Pokas <reddev0110@gmail.com>
2024-12-11 23:03:54 +0900

    Initial Update

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'lukem75025@gmail.com',
+    phone: '+1 352 682 2750',
+    address: 'United States, Florida, Gainesville',
+    github: 'https://github.com/mdev0110',
+    facebook: 'https://www.facebook.com/',
+    linkedIn: 'https://www.linkedin.com/',
+    twitter: 'https://twitter.com/',
+    stackOverflow: 'https://stackexchange.com/users/33700505/ace-dev',
+    devUsername: "acedev0110"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "GatorDev Agency",
+  profile: '/profile.png',
+  designation: "GatorDev Agency",
+  description: "We are senior software development agency. We are professional and enthusiastic programmers in our daily lives. We are quick learners with a self-learning attitude. We love to learn and explore new technologies and are passionate about problem-solving. We embrace almost all the stacks of web application development and are dedicated to making the web more open to the world. Our core skill is based on JavaScript, and we love to accomplish most tasks using it. We are available for any kind of job opportunity that suits our skills and interests.",
+  email: 'lukem75025@gmail.com',
+  phone: '+1 352 682 2750',
+  address: 'USA, Florida, Gainesville ',
+  github: 'https://github.com/mdev0110',
+  facebook: 'https://www.facebook.com/',
+  linkedIn: 'https://www.linkedin.com/',
+  twitter: 'https://twitter.com/',
+  stackOverflow: 'https://stackexchange.com/users/33700505/ace-dev',
+  leetcode: "https://leetcode.com/",
+  devUsername: "acedev0110",
+  resume: "https://drive.google.com/"
+}
